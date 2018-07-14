15ccf49256d4d8a51f63552546a890d53f11ad06
m83Dragan <sergioxyzribeiro@gmail.com>
2018-07-14 19:37:02 +0800

    create react-redux sample

---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+import todos from './todos';
+import visibilityFilter from './visibilityFilter';
+const rootReducer = combineReducers({ todos, visibilityFilter });
+export default rootReducer;
